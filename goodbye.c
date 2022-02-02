7575d4497347302c71e83a5d79998e97682123ed
Pedro Américo Vinhas <pedrovinhas@protonmail.com>
2022-02-02 11:45:14 -0300

    novo teste em C e adicionando .gitignore

---
+++ goodbye.c
@@ -0,0 +1,13 @@
+/*
+ * goodbye.c
+ * Copyright (C) 2022 Pedro Vinhas <pedrovinhas@pm.me>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Goodbye cruel world\n");
+}
